f253617188583b97e8eea07fbdfb541b6de14ac6
Benjamin Zou <143123593+bwz5@users.noreply.github.com>
2024-01-24 19:20:22 -0500

    Delete build directory

--- build/CMakeFiles/open_terminal.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/open_terminal"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/open_terminal.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
